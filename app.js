5a8c2b6349d820cf677bea78d090d51a5d7b8ab0
Eddie Borja <eddie@Eddies-MBP.frontierlocal.net>
2018-12-22 19:13:29 -0800

    setting up UI

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function(){
+
+
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('clicked!!!')
+    })
+
+})(budgetController, UIController);
+
