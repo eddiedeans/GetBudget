6d475c74134363bfaffb62222f91f908865ee792
Eddie Borja <eddie@Eddies-MBP.frontierlocal.net>
2018-12-22 20:02:44 -0800

    getting values and setting up handlers

--- app.js
+++ app.js
@@ -9,16 +9,58 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
 
+    }
+
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        console.log('clicked!!!')
-    })
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+        // get the user value
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // add the item to the budget controller
+
+        // add the item to the UI
+
+        // calculate the budget
+
+        // display the budget on UI
+
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if (event.keycode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
 
