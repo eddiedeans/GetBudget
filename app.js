2f790a2674fb354308c60f831091538e81d7d268
Eddie Borja <eddie@Eddies-MBP.frontierlocal.net>
2018-12-23 12:18:22 -0800

    delete is working for data structure

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ var budgetController = (function(){
 
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+            var ids = data.allItems[type].map(function (current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         calculateBudget: function(){
 
             // calcuate total income and expenses
@@ -249,10 +263,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split("-");
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]) ;
 
             // delete item from data structure
-
+            budgetCtrl.deleteItem(type, ID);
             // delete the item from the UI
 
             // update and show the new budget
