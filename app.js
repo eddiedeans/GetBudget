e08746b0bee192bdd94931a38922115216628b92
Eddie Borja <eddiebor@yahoo.com>
2018-12-26 19:35:44 -0800

    formatted the date for every month possible

--- app.js
+++ app.js
@@ -155,7 +155,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     }
 
@@ -273,6 +274,19 @@ var UIController = (function() {
 
         },
 
+        displayMonth: function() {
+            var now, year, month, months
+
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ", " + year;
+        },
+
 
         getDOMstrings: function() {
             return DOMstrings;
@@ -379,6 +393,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc:  0,
